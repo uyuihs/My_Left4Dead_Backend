fc8e0d1c38276a6108d21ac21571109f51b83093
shiyu <shiyu039@outlook.com>
2025-05-16 16:05:05 +0800

    配置nginx

--- My_Left4Dead_Backend/settings.py
+++ My_Left4Dead_Backend/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
-from My_Left4Dead_Backend.host import host
+from My_Left4Dead_Backend.host import host, host2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,13 +26,13 @@ SECRET_KEY = 'django-insecure-%cyfqiugnkb&tu67rh@+&*a$x&(4^wcn$+3d@yp2e*xx22ioiw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [host]
+ALLOWED_HOSTS = [host, host2]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'game',
+    'game.apps.GameConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -119,7 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
