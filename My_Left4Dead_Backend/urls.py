fb3383ca10ec451efbc3f5cfabb3787b35dd6c6e
shiyu <shiyu039@outlook.com>
2025-05-16 17:00:26 +0800

    搭建项目骨架

--- My_Left4Dead_Backend/urls.py
+++ My_Left4Dead_Backend/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-   # path('game/', include('game.urls')),
+    path('', include("game.urls.index")),
     path('admin/', admin.site.urls),
 ]
